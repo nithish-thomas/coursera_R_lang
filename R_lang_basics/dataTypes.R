3ed317b0e571be1d0d509d81d9cdea8d461b12de
nithish <nithish@localhost>
2014-12-21 17:18:27 +0530

    R lang basics initial commit

---
+++ R_lang_basics/dataTypes.R
@@ -0,0 +1,13 @@
+x<-factor('1','2','4')
+unclass(x)
+x<-factor(c(1,2,3,4))
+x
+x<-factor()
+x
+factor(1,2,4,5)
+yx<-data.fram(c(1:4),c(T,F,F,T))
+yx<-data.frame(c(1:4),c(T,F,F,T))
+yx
+nrow(yx)
+ncol(yx)
+savehistory("~/git/coursera_R_lang/Untitled Folder/dataTypes.Rhistory")
