8e67b4de41f0aaf3f0abfaaa7842b04b20d4e6fb
nithish <nithish@localhost>
2015-01-10 21:05:11 +0530

    Duplicate challenge completed

--- LeetCode/src/com/nst/challenger/RemoveDuplicate.java
+++ LeetCode/src/com/nst/challenger/RemoveDuplicate.java
@@ -1,9 +1,40 @@
 package com.nst.challenger;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
 public class RemoveDuplicate {
 
 	public RemoveDuplicate() {
 		// TODO Auto-generated constructor stub
 	}
 
+	
+	public static void main(String[] args) {
+		float[] test=new float[]{1.00f,3.00f,5.45f,1.00f};
+		RemoveDuplicate d=new RemoveDuplicate();
+		
+		d.removeDup(test);
+	}
+	
+	public void removeDup(float[] a){
+		Set<Float> duplicate=new HashSet<Float>(a.length);
+		Set<Float> unique=new HashSet<>(a.length);
+		
+		for (Float data : a) {
+			if(unique.contains(data)){
+				duplicate.add(data);
+			}else{
+				unique.add(data);
+			}
+		}
+		System.out.println("Duplicate : "+duplicate);
+		System.out.println("Unique :"+unique);
+	}
+	
 }
